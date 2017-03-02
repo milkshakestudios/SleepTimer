a0509383de58747befe24cc792ee287154bcefe9
milkshakestudios <milkshakestudios1@gmail.com>
2017-03-02 00:53:09 -0500

    Logic change for calculateColorZoneTillCurrentTime()   -Edge cases fail

--- app/src/main/java/com/milkshakestudio/sleeptimer/sleeptimer/MainActivity.java
+++ app/src/main/java/com/milkshakestudio/sleeptimer/sleeptimer/MainActivity.java
@@ -17,165 +17,199 @@ import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
-    private int timeHours;
-    private int timeFromPrefs;
 
+    private static final int MAXHOUR_INDAY = 23;
     private static String HOUR_KEY = "hourKey";
-    private int a = 1;
+    int incrementalTime;
+    ValueAnimator animator;
+    private int timeFromPrefs;
+    private int timeCurrent;
+    private ArrayList<Integer> arrayList = new ArrayList<Integer>();
 
-    private ArrayList<Integer> colorList;
+    public static int[] convertIntegers(ArrayList<Integer> integers)
+    {
+        int[] ret = new int[integers.size()];
+        for (int i=0; i < ret.length; i++)
+        {
+            ret[i] = integers.get(i).intValue();
+        }
+        return ret;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        timeHours = Calendar.getInstance().getTime().getHours();
-
-
-        SharedPreferences sharedPreferences = getSharedPreferences("sleepTimer",MODE_PRIVATE);
-
-
-        timeFromPrefs = sharedPreferences.getInt(HOUR_KEY,-1);
-
-        sharedPreferences.edit().putInt(HOUR_KEY,timeHours).apply();
+        timeFromPrefs = loadTimeFromPreferences();
+        timeCurrent = fetchCurrentTime();
 
         TextView timeField = (TextView) findViewById(R.id.timeField);
-
-
-        timeField.setText(""+ timeHours%12);
-//        timeField.setText(Calendar.getInstance().getTime().getHours());
-
-
-
+        timeField.setText(getString(R.string.currentHour,String.valueOf(timeCurrent)));
 
         AppCompatButton startButton = (AppCompatButton) findViewById(R.id.start_stop_btn);
         startButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                changeState();
+                if(!animator.isRunning()) {
+                    animator.start();
+                }
             }
         });
 
-//        SeekArc arc = (SeekArc) findViewById(R.id.seekArc);
-//        arc.setStartAngle(0);
-//        arc.setSweepAngle(360);
-//        NumberPicker numberPicker = (NumberPicker) findViewById(R.id.number_picker);
-//        numberPicker.setMinValue(0);
-//        numberPicker.setMaxValue(60);
-//        numberPicker.setWrapSelectorWheel(true);
 
-        setupColor();
+        //DEBUG
+        timeFromPrefs = 20;
+        timeCurrent = 5;
+
+        setUpScrollColorsAndAnimate();
 
     }
 
-    private void setupColor() {
-        colorList = new ArrayList<Integer>();
+    private void setUpScrollColorsAndAnimate() {
 
-        colorList.add(getResources().getColor(R.color.colorVeryEarlyMorning));
-    }
-
-
-    private void changeState() {
-
-        int colorFrom = setStartColor();
-
-        int colorTo = setEndColor();
+        int colorBasedOnLastLoginTime = getColorValueBasedOnHour(timeFromPrefs);
 
         final RelativeLayout relativeLayout = (RelativeLayout) findViewById(R.id.activity_main);
         final Window window = this.getWindow();
-
-// clear FLAG_TRANSLUCENT_STATUS flag:
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
-
-// add FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS flag to the window
         window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
 
+        relativeLayout.setBackgroundColor(colorBasedOnLastLoginTime);
+        window.setStatusBarColor(colorBasedOnLastLoginTime);
 
-        ValueAnimator colorAnimation = new ValueAnimator();
-        colorAnimation.setIntValues(colorFrom,colorTo);
-        colorAnimation.setEvaluator(new ArgbEvaluator());
+        if(timeFromPrefs == timeCurrent){
+            return;
+        }
 
-        colorAnimation.setDuration(2500); // milliseconds
+        incrementalTime = timeFromPrefs;
+        arrayList.add(colorBasedOnLastLoginTime);
+        calculateColorZonesTillCurrentTime(timeFromPrefs);
 
-        colorAnimation.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
-
+        ArgbEvaluator evaluator = new ArgbEvaluator();
+        animator = new ValueAnimator();
+        animator.setIntValues(convertIntegers(arrayList));
+        animator.setEvaluator(evaluator);
+        animator.setDuration(10000);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
-            public void onAnimationUpdate(ValueAnimator animator) {
-                relativeLayout.setBackgroundColor((int) animator.getAnimatedValue());
-                window.setStatusBarColor((int) animator.getAnimatedValue());
+            public void onAnimationUpdate(ValueAnimator animation) {
+                relativeLayout.setBackgroundColor((int) animation.getAnimatedValue());
+                window.setStatusBarColor((int) animation.getAnimatedValue());
             }
-
         });
-        colorAnimation.start();
-
     }
 
-    private int setStartColor() {
-        int colorStart = getResources().getColor(R.color.colorPrimaryDark);
+    boolean dayRolledOver = false;
+    private void calculateColorZonesTillCurrentTime(int timeSaved) {
+//        if(!dayRolledOver) {
+//            if (timeSaved == timeCurrent) {
+//                arrayList.add(getColorValueBasedOnHour(timeSaved));
+//                return;
+//            }
+//
+//            if (timeSaved > MAXHOUR_INDAY) {
+////                timeSaved = timeSaved - MAXHOUR_INDAY - 1;
+//                dayRolledOver = true;
+//                calculateColorZonesTillCurrentTime(timeSaved);
+//            }
+//
+//            if (timeSaved <= timeCurrent) {
+//                arrayList.add(getColorValueBasedOnHour(timeSaved));
+//                timeSaved += 3;
+//                calculateColorZonesTillCurrentTime(timeSaved);
+//            } else {
+//                if (((float) timeSaved - (float) timeCurrent) / 3F > 1) {
+//                    return;
+//                } else {
+//                    arrayList.add(getColorValueBasedOnHour(timeSaved));
+//                }
+//            }
+//        }else{
+//            if(timeCurrent < timeSaved){
+//                return;
+//            }
+//        }
 
-        if(timeFromPrefs == -1){
-        }else if(timeFromPrefs >= 0 && timeFromPrefs <= 2){     //12am  to 3am
-            colorStart = getResources().getColor(R.color.colorVeryEarlyMorning);
-        }else if(timeFromPrefs >= 3 && timeFromPrefs <= 5){     //3am   to 6am
-            colorStart = getResources().getColor(R.color.colorEarlyMorning);
-        }else if(timeFromPrefs >= 6 && timeFromPrefs <= 8){     //6am   to 9am
-            colorStart = getResources().getColor(R.color.colorMorning);
-        }else if(timeFromPrefs >= 9 && timeFromPrefs <= 11){    //9am   to 12pm
-            colorStart = getResources().getColor(R.color.colorMidMorning);
-        }else if(timeFromPrefs >= 12 && timeFromPrefs <= 14){    //12pm   to 3pm
-            colorStart = getResources().getColor(R.color.colorAfternoon);
-        }else if(timeFromPrefs >= 15 && timeFromPrefs <= 17){    //3pm   to 6pm
-            colorStart = getResources().getColor(R.color.colorEarlyEvening);
-        }else if(timeFromPrefs >= 18 && timeFromPrefs <= 20){    //6pm   to 9pm
-            colorStart = getResources().getColor(R.color.colorEvening);
-        }else if(timeFromPrefs >= 21 && timeFromPrefs <= 23){    //9pm   to 12am
-            colorStart = getResources().getColor(R.color.colorMidnight);
-        }else{
-            colorStart = getResources().getColor(R.color.colorPrimaryDark);
+        if(timeSaved == timeCurrent){
+            arrayList.add(getColorValueBasedOnHour(timeSaved));
+            return;
         }
-        return colorStart;
-    }
 
-    private int setEndColor() {
-        int colorEnd = getResources().getColor(R.color.colorMidnight);
-
-        if(timeHours == -1){
-        }else if(timeHours >= 0 && timeHours <= 2){     //12am  to 3am
-            colorEnd = getResources().getColor(R.color.colorVeryEarlyMorning);
-        }else if(timeHours >= 3 && timeHours <= 5){     //3am   to 6am
-            colorEnd = getResources().getColor(R.color.colorEarlyMorning);
-        }else if(timeHours >= 6 && timeHours <= 8){     //6am   to 9am
-            colorEnd = getResources().getColor(R.color.colorMorning);
-        }else if(timeHours >= 9 && timeHours <= 11){    //9am   to 12pm
-            colorEnd = getResources().getColor(R.color.colorMidMorning);
-        }else if(timeHours >= 12 && timeHours <= 14){    //12pm   to 3pm
-            colorEnd = getResources().getColor(R.color.colorAfternoon);
-        }else if(timeHours >= 15 && timeHours <= 17){    //3pm   to 6pm
-            colorEnd = getResources().getColor(R.color.colorEarlyEvening);
-        }else if(timeHours >= 18 && timeHours <= 20){    //6pm   to 9pm
-            colorEnd = getResources().getColor(R.color.colorEvening);
-        }else if(timeHours >= 21 && timeHours <= 23){    //9pm   to 12am
-            colorEnd = getResources().getColor(R.color.colorMidnight);
-        }else{
-            colorEnd = getResources().getColor(R.color.colorPrimaryDark);
+        if(timeSaved > MAXHOUR_INDAY){
+            timeSaved = timeSaved - MAXHOUR_INDAY - 1;
+            dayRolledOver = true;
         }
-        return colorEnd;
+
+
+        if(timeSaved < timeCurrent && !dayRolledOver){
+            arrayList.add(getColorValueBasedOnHour(timeSaved));
+            timeSaved+=3;
+            calculateColorZonesTillCurrentTime(timeSaved);
+        }else {
+            arrayList.add(getColorValueBasedOnHour(timeSaved));
+            timeSaved += 3;
+            calculateColorZonesTillCurrentTime(timeSaved);
+        }
+
     }
 
 
-//    @Override
-//    public void onWindowFocusChanged(boolean hasFocus) {
-//        super.onWindowFocusChanged(hasFocus);
-//        if (hasFocus) {
-//            getWindow().getDecorView().setSystemUiVisibility(
-//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-//                            | View.SYSTEM_UI_FLAG_FULLSCREEN
-//                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
-//    }
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            getWindow().getDecorView().setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }
+
+
+    private int getColorValueBasedOnHour(int timeCurrent) {
+        int colorCurrent;
+
+        if(timeCurrent == -1){
+            colorCurrent = getResources().getColor(R.color.colorPrimary);
+        }else if(timeCurrent >= 0 && timeCurrent <= 2){     //12am  to 3am
+            colorCurrent = getResources().getColor(R.color.colorVeryEarlyMorning);
+        }else if(timeCurrent >= 3 && timeCurrent <= 5){     //3am   to 6am
+            colorCurrent = getResources().getColor(R.color.colorEarlyMorning);
+        }else if(timeCurrent >= 6 && timeCurrent <= 8){     //6am   to 9am
+            colorCurrent = getResources().getColor(R.color.colorMorning);
+        }else if(timeCurrent >= 9 && timeCurrent <= 11){    //9am   to 12pm
+            colorCurrent = getResources().getColor(R.color.colorMidMorning);
+        }else if(timeCurrent >= 12 && timeCurrent <= 14){    //12pm   to 3pm
+            colorCurrent = getResources().getColor(R.color.colorAfternoon);
+        }else if(timeCurrent >= 15 && timeCurrent <= 17){    //3pm   to 6pm
+            colorCurrent = getResources().getColor(R.color.colorEarlyEvening);
+        }else if(timeCurrent >= 18 && timeCurrent <= 20){    //6pm   to 9pm
+            colorCurrent = getResources().getColor(R.color.colorEvening);
+        }else if(timeCurrent >= 21 && timeCurrent <= 23){    //9pm   to 12am
+            colorCurrent = getResources().getColor(R.color.colorMidnight);
+        }else{
+            colorCurrent = getResources().getColor(R.color.colorPrimary);
+        }
+        return colorCurrent;
+    }
+
+
+    private int loadTimeFromPreferences() {
+        SharedPreferences sharedPreferences = getSharedPreferences("sleepTimer",MODE_PRIVATE);
+        timeFromPrefs = sharedPreferences.getInt(HOUR_KEY,-1);
+//        if(timeFromPrefs==-1){
+//            return fetchCurrentTime();
+//        }
+        return timeFromPrefs;
+    }
+    private int fetchCurrentTime() {
+        int currentTime = Calendar.getInstance().getTime().getHours();
+//        SharedPreferences sharedPreferences = getSharedPreferences("sleepTimer",MODE_PRIVATE);
+//        sharedPreferences.edit().putInt(HOUR_KEY,currentTime).apply();
+        return currentTime;
+    }
 
 }
